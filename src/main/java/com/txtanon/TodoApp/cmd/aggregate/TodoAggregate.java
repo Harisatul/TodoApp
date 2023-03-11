a570db237940917bf8200530c88fa32afcc3d72a
haris <haris.aulia404@gmail.com>
2023-03-11 14:14:31 +0700

    added CreateTodoCommand handler

--- src/main/java/com/txtanon/TodoApp/cmd/aggregate/TodoAggregate.java
+++ src/main/java/com/txtanon/TodoApp/cmd/aggregate/TodoAggregate.java
@@ -1,8 +1,12 @@
 package com.txtanon.TodoApp.cmd.aggregate;
 
 
+import com.txtanon.TodoApp.cmd.commands.CreateTodoCommand;
+import com.txtanon.TodoApp.cmd.core.events.TodoCreateEvent;
 import com.txtanon.TodoApp.cmd.core.models.Todo;
+import org.axonframework.commandhandling.CommandHandler;
 import org.axonframework.modelling.command.AggregateIdentifier;
+import org.axonframework.modelling.command.AggregateLifecycle;
 import org.axonframework.spring.stereotype.Aggregate;
 
 @Aggregate
@@ -15,5 +19,16 @@ public class TodoAggregate {
     public TodoAggregate() {
     }
 
+    @CommandHandler
+    public TodoAggregate(CreateTodoCommand command) {
+        Todo todo = command.getTodo();
+        todo.setId(command.getId());
+
+        TodoCreateEvent todoBuild = TodoCreateEvent.builder()
+                .id(command.getId())
+                .todo(todo)
+                .build();
+        AggregateLifecycle.apply(todoBuild);
+    }
 
 }
