aa1f99480d39a23c5bf5607de35ba42d76d0cbbc
haris <haris.aulia404@gmail.com>
2023-03-11 17:29:59 +0700

    defined TodoRepository

---
+++ src/main/java/com/txtanon/TodoApp/cmd/repositories/TodoRepository.java
@@ -0,0 +1,10 @@
+package com.txtanon.TodoApp.cmd.repositories;
+
+import com.txtanon.TodoApp.cmd.core.models.Todo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TodoRepository extends JpaRepository<String, Todo> {
+
+}
